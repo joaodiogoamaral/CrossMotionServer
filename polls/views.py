19a81148ca55a1a049bafcc484bbee8ebb69f798
ieeta <danielduartecanedo@ua.pt>
2019-03-13 23:28:14 +0000

    Just a comment

--- polls/views.py
+++ polls/views.py
@@ -6,6 +6,6 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-
+#lord amaral
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
